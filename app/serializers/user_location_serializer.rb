bd02e7d0feb1a874d0923a9200104fdc9cef3005
atang-sp <atang-sp@users.noreply.github.com>
2025-07-15 20:19:00 +0800

    修复附近朋友列表显示问题
    - 修复本地化问题，性别正确显示中文
    - 只显示性别和属性两个自定义字段
    - 移除多余的用户信息显示（bio、年龄、姓名、标题、加入时间等）
    - 简化用户卡片，保持界面简洁

--- app/serializers/user_location_serializer.rb
+++ app/serializers/user_location_serializer.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class UserLocationSerializer < ApplicationSerializer
-  attributes :id, :username, :name, :avatar_template, :distance, :last_seen_at, 
-             :is_virtual, :virtual_address, :location_type, :gender, :age, :bio, 
-             :user_fields, :title, :created_at, :location_display_name, :is_online
+  attributes :id, :username, :name, :avatar_template, :distance,
+             :is_virtual, :virtual_address, :location_type, :gender, 
+             :user_fields, :location_display_name, :is_online
 
   def id
     object[:user].id
@@ -25,9 +25,7 @@ class UserLocationSerializer < ApplicationSerializer
     object[:distance]
   end
 
-  def last_seen_at
-    object[:user].last_seen_at
-  end
+
 
   def is_virtual
     object[:location].is_virtual || false
@@ -60,82 +58,37 @@ class UserLocationSerializer < ApplicationSerializer
     gender_value
   end
 
-  def age
-    # 尝试从用户字段中获取年龄信息
-    user_fields_data = object[:user].user_fields || {}
-    
-    age_value = user_fields_data['年龄'] || 
-                user_fields_data['age'] || 
-                user_fields_data['Age'] ||
-                user_fields_data['生日'] ||
-                user_fields_data['birthday']
-    
-    # 如果用户字段中没有年龄信息，尝试从自定义字段获取
-    if age_value.blank?
-      age_value = object[:user].custom_fields&.dig('age')
-    end
-    
-    return nil if age_value.blank?
-    
-    # 如果是生日格式，计算年龄
-    if age_value.to_s.match(/\d{4}/)
-      begin
-        birth_year = age_value.to_s.scan(/\d{4}/).first.to_i
-        current_year = Date.current.year
-        calculated_age = current_year - birth_year
-        return calculated_age if calculated_age > 0 && calculated_age < 150
-      rescue
-        # 如果计算失败，继续处理原值
-      end
-    end
-    
-    # 如果是数字字符串，转换为整数
-    if age_value.to_s.match(/^\d+$/)
-      age_int = age_value.to_i
-      return age_int if age_int > 0 && age_int < 150
-    end
-    
-    # 返回原始值（可能是其他格式的年龄表示）
-    age_value.to_s
-  end
 
-  def bio
-    object[:user].user_profile&.bio_raw
-  end
 
   def user_fields
-    # 返回所有用户自定义字段
+    # 只返回"属性"字段
     fields = {}
     
     # 获取用户字段数据
     if object[:user].user_fields.present?
       object[:user].user_fields.each do |key, value|
         next if value.blank?
-        # 排除已经单独处理的字段
-        next if ['性别', 'gender', 'Gender', 'sex', 'Sex', '年龄', 'age', 'Age', '生日', 'birthday'].include?(key)
-        fields[key] = value
+        # 只包含"属性"字段
+        if ['属性', 'attributes', 'Attributes'].include?(key)
+          fields[key] = value
+        end
       end
     end
     
-    # 获取自定义字段
+    # 获取自定义字段中的属性
     if object[:user].custom_fields.present?
       object[:user].custom_fields.each do |key, value|
         next if value.blank?
-        next if ['gender', 'age'].include?(key)
-        fields[key] = value
+        if ['属性', 'attributes', 'Attributes'].include?(key)
+          fields[key] = value
+        end
       end
     end
     
     fields
   end
 
-  def title
-    object[:user].title
-  end
 
-  def created_at
-    object[:user].created_at
-  end
 
   def location_display_name
     if is_virtual && virtual_address.present?
